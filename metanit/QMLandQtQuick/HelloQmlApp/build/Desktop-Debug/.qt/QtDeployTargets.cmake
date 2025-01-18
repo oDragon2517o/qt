717650b494d1ee672f2d2f206110ada6264c6bec
Dragon2517 <evseev.vasya@yandex.ru>
2025-01-18 15:51:46 +0300

    222

---
+++ metanit/QMLandQtQuick/HelloQmlApp/build/Desktop-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_appHelloQmlApp_FILE C:/Rab/qt/metanit/QMLandQtQuick/HelloQmlApp/build/Desktop-Debug/appHelloQmlApp.exe)
+set(__QT_DEPLOY_TARGET_appHelloQmlApp_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_appHelloQmlApp_RUNTIME_DLLS C:/Qt/6.8.1/msvc2022_64/bin/Qt6Quickd.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6QmlMetad.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6QmlWorkerScriptd.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6QmlModelsd.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Qmld.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Networkd.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6OpenGLd.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Cored.dll)
